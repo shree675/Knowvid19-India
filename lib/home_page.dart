9f1755d8e7f29fe5c5d2f6075cd005b921ebc340
shree675 <shreeteshm37@gmail.com>
2021-02-01 19:49:41 +0530

    started hero banner

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,6 +8,68 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(""),
+        backgroundColor: Color(0xff4d4dff),
+          elevation: 0.0,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              Container(
+                color: Color(0xff4d4dff),
+                // color: Color(0xff454545),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.vertical(
+                    bottom: Radius.circular(50.0),        // 50 to 60
+                  ),
+                ),
+                height: 300.0,                            // make it dynamic and responsive
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text('Location',
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        color: Colors.amber,              // maybe white or green
+                        fontWeight: FontWeight.bold,
+                      ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 20.0),
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 8.0, vertical: 8.0),
+                        height: 60.0,
+                        alignment: Alignment.bottomCenter,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(30.0),
+                            border: Border.all(color: Colors.greenAccent,)
+                        ),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Icon(Icons.location_on_rounded,
+                              color: Colors.greenAccent,),
+                            Expanded(
+                              // child: ,
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
